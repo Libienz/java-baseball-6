bf1b488217649d2c5be51bc631559404a6a01108
libienz <Leekh10195@gmail.com>
2023-12-03 15:36:45 +0900

    feat: 메인 컨트롤러 구현

---
+++ src/main/java/baseball/controller/MainController.java
@@ -0,0 +1,43 @@
+package baseball.controller;
+
+import baseball.domain.Balls;
+import baseball.domain.ContinueMode;
+import baseball.dto.ScoreDto;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class MainController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final ComputerNumberController computerNumberController;
+    private final ScoreCalculationController scoreCalculationController;
+
+    public MainController(InputView inputView, OutputView outputView, ComputerNumberController computerNumberController, ScoreCalculationController scoreCalculationController) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.computerNumberController = computerNumberController;
+        this.scoreCalculationController = scoreCalculationController;
+    }
+
+    public void run() {
+        ContinueMode continueMode = ContinueMode.CONTINUE;
+        outputView.printGreetingMessage();
+
+        while (continueMode == ContinueMode.CONTINUE) {
+            Balls computerBalls = computerNumberController.generateComputerNumber();
+            int strikeCount = 0;
+            while (strikeCount != 3) {
+                Balls userBalls = inputView.inputBalls();
+                ScoreDto scoreDto = scoreCalculationController.calculateScore(userBalls, computerBalls);
+                outputView.printResultMessage(scoreDto);
+
+                strikeCount = scoreDto.getStrikeCount();
+
+            }
+            outputView.printWinMessage();
+            continueMode = inputView.inputContinueMode();
+        }
+
+    }
+}
