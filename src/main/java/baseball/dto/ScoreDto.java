d0d2564f10045e1b38327c133741a3e1f1a22666
libienz <Leekh10195@gmail.com>
2023-12-03 13:50:21 +0900

    refactor: ScoreDto 생성 로직 정적 팩토리 메서드로 통일

--- src/main/java/baseball/dto/ScoreDto.java
+++ src/main/java/baseball/dto/ScoreDto.java
@@ -4,9 +4,13 @@ public class ScoreDto {
     private final int strikeCount;
     private final int ballCount;
 
-    public ScoreDto(int strikeCount, int ballCount) {
+    private ScoreDto(int strikeCount, int ballCount) {
         this.strikeCount = strikeCount;
         this.ballCount = ballCount;
+    }
+
+    public static ScoreDto of(int strikeCount, int ballCount) {
+        return new ScoreDto(strikeCount, ballCount);
     }
 
     public int getStrikeCount() {
